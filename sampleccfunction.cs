d7c5d249b21338be49c77e49e95b09d13b82ce0f
medammar <medammar200@gmail.com>
2019-09-04 14:45:21 +0100

    Add files via upload

---
+++ sampleccfunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class samplecc : MonoBehaviour {
+int a = 7;
+int b = 8;
+int c;
+int f;
+float e;
+public void calcul ()
+{
+    c = a + b; 
+	f =	a * b;
+	e = (float) a/b;
+	 Debug.Log(c);
+	 Debug.Log(f);
+	 Debug.Log(e); 
+}
+
+	// Use this for initialization
+	void Start () {
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+	}
+}
